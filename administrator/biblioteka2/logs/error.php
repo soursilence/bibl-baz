804b628a3bd0aa27082d47827c7eac6424b57946
tkaniowski <tomasz.kaniowski@gmail.com>
2017-02-05 15:46:57 +0100

    cleaning j3

--- administrator/biblioteka2/logs/error.php
+++
@@ -1,8 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2016-03-11 17:30:30 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2016-03-11T17:30:30+00:00	INFO 78.11.179.179	joomlafailure	Nieprawidłowa nazwa użytkownika lub hasło albo nie masz u nas jeszcze konta.
-2016-03-11T17:30:45+00:00	INFO 78.11.179.179	joomlafailure	Nieprawidłowa nazwa użytkownika lub hasło albo nie masz u nas jeszcze konta.
